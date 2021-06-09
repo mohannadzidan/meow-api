ec34615ec8cd0fe6e1ccf82448248428038ae64a
Mohannad Zidan <moh.anad23823@gmail.com>
2021-06-09 09:16:40 +0200

    add delete method to cors

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(function (req, res, next) {
     // for debugging purpose...
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
     res.setHeader("Access-Control-Allow-Headers", "*");
     if (req.method === "OPTIONS") {
         res.status(200).send();
