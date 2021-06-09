cb3c8ec497dbe9486a8ab3c39900c3587828dbc0
Mohannad Zidan <moh.anad23823@gmail.com>
2021-06-09 09:16:04 +0200

    update queries

--- modules/newsfeed.js
+++ modules/newsfeed.js
@@ -1,6 +1,6 @@
 var auth = require('./auth.js');
 var { requireRequestBody } = require('../schema.js');
-var { db } = require('../db');
+var { db, PrismaErrorCodes } = require('../db');
 var convertParams = require('../query-params').convertParams;
 const { respondWithError, Errors, handleInternalError } = require('../errors.js');
 const { routeObjectFunctions } = require('../middleware/common.js');
@@ -34,7 +34,6 @@ const commentCreationSchema = {
 
 };
 
-
 var posts = {
     /**
      * http://[host]/api/newsfeed/posts:method
@@ -61,10 +60,11 @@ var posts = {
                         result.sharesCount = 0;
                         result.likesCount = 0;
                         result.commentsCount = 0;
+                        result.liked = false;
                         result.timestamp = Math.floor(result.timestamp.getTime() / 1000);
                         res.status(201).json(result);
                     }).catch(e => {
-                        if (e.code === 'P2003' && e.meta.field_name === 'sharedPostId') {
+                        if (e.code === PrismaErrorCodes.FOREIGN_KEY_CONSTRAINT_FAILED) {
                             respondWithError(res, Errors.POST_NOT_FOUND, { target: 'sharedPostId' });
                         } else
                             handleInternalError(res, e);
@@ -100,7 +100,7 @@ var posts = {
                 if (post.userId !== req.auth.id) {
                     return Promise.reject(Errors.PERMISSION_DENIED);
                 }
-                return db.post.delete({ where: { id: postId } });
+                return db.$queryRaw`DELETE FROM post WHERE id =${postId};`;
             }).then(() => {
                 res.status(204).json();
             }).catch(e => {
@@ -125,12 +125,13 @@ var posts = {
             if (postId === undefined) {
                 return respondWithError(res, Errors.POST_NOT_FOUND);
             }
-            db.post_view.findFirst({
-                where: { id: postId }
-            }).then(post => {
-                if (!post) {
+
+            db.$queryRaw`SELECT *, EXISTS(select postId from post_like WHERE userId = ${req.auth.id} AND postId = ${postId}) as liked FROM post_view where id = ${postId} LIMIT 1;`.then(post => {
+                if (!post || post.length === 0) {
                     return Promise.reject(Errors.POST_NOT_FOUND);
                 }
+                post = post[0];
+                post.liked = post.liked ? true : false;
                 res.status(200).json(post);
             }).catch(e => {
                 if (e === Errors.POST_NOT_FOUND) {
@@ -150,6 +151,7 @@ var posts = {
         if (requireParams(req, res, { before: 'integer?', after: 'integer?' })) {
             const before = req.query.before ?? Number.MAX_SAFE_INTEGER;
             const after = req.query.after ?? 0;
+
             // get all followings
             db.follow.findMany({
                 select: { secondId: true }, where: { firstId: req.auth.id }
@@ -157,18 +159,10 @@ var posts = {
                 // remap into set
                 const idSet = result.map((x => x.secondId));
                 idSet.push(req.auth.id) // push self id
-                return db.post_view.findMany({
-                    where: {
-                        userId: { in: idSet }, AND: {
-                            timestamp: { lt: before },
-                            AND: {
-                                timestamp: { gt: after }
-                            }
-                        }
-                    },
-                    take: 10,
-                });
+                const query = `SELECT *, EXISTS(select postId from post_like WHERE post_like.userId = ${req.auth.id} AND post_like.postId = id) as liked FROM post_view where post_view.userId IN (${idSet.join()}) AND post_view.timestamp < ${before} AND post_view.timestamp > ${after} ORDER BY post_view.timestamp DESC LIMIT 10;`;
+                return db.$queryRaw(query);
             }).then(posts => {
+                posts.forEach(post => post.liked = post.liked ? true : false);
                 res.status(200).json(posts);
             }).catch(e => handleInternalError(res, e));
         }
@@ -177,7 +171,7 @@ var posts = {
 }
 
 var comments = {
-    commentId: {
+    id: {
         /**
          * 
          * http://[host]/api/posts/{postId/comments/{commentId} Method DELETE
@@ -228,15 +222,10 @@ var comments = {
             if (postId === undefined) {
                 return respondWithError(res, Errors.POST_NOT_FOUND);
             }
-            db.comment_view.findFirst({
-                where: {
-                    id: commentId,
-                    AND: {
-                        postId: postId,
-                    },
-                }
-            }).then(comment => {
-                if (!comment) return Promise.reject(Errors.COMMENT_NOT_FOUND);
+            db.$queryRaw(`SELECT *, EXISTS(SELECT commentId from comment_like where comment_like.userId = comment_view.userId AND comment_like.commentId = comment_view.id) as liked from comment_view where id = ${commentId} limit 1;`).then(comment => {
+                if (!comment || comment.length === 0) return Promise.reject(Errors.COMMENT_NOT_FOUND);
+                comment = comment[0];
+                comment.liked = comment.liked === 1;
                 res.status(200).json(comment);
             }).catch(e => {
                 if (e === Errors.COMMENT_NOT_FOUND) {
@@ -247,7 +236,6 @@ var comments = {
             });
         }
     },
-
 
     /**
     * 
@@ -263,25 +251,7 @@ var comments = {
             respondWithError(res, Errors.POST_NOT_FOUND);
             return;
         }
-        db.comment_view.findMany({
-            where: {
-                postId: postId,
-                AND: {
-                    timestamp: {
-                        lt: before,
-                    },
-                    AND: {
-                        timestamp: {
-                            gt: after,
-                        }
-                    }
-                }
-            },
-            take: 10,
-            orderBy: {
-                timestamp: 'desc'
-            }
-        }).then(comments => {
+        db.$queryRaw(`SELECT *, EXISTS(SELECT commentId from comment_like where comment_like.userId = comment_view.userId AND comment_like.commentId = comment_view.id) as liked from comment_view where postId = ${postId} AND timestamp < ${before} AND timestamp > ${after} ORDER BY timestamp DESC limit 10;`).then(comments => {
             res.status(200).json(comments);
         }).catch(e => {
             if (e === Errors.COMMENT_NOT_FOUND) {
@@ -311,6 +281,7 @@ var comments = {
                         data: req.body
                     }).then(comment => {
                         comment.likesCount = 0;
+                        comment.liked=false;
                         comment.timestamp = Math.floor(comment.timestamp.getTime() / 1000);
                         res.status(201).json(comment);
                     }).catch(e => {
@@ -415,8 +386,92 @@ var comments = {
     }
 }
 
+let likes = {
+    post: {
+        methods: {
+            POST: {
+                /**
+                * 
+                * @param {import('express').Request} req 
+                * @param {import('express').Response} res 
+                */
+                toggle: function (req, res) {
+                    const postId = converters.integer(req.params.postId);
+                    if (postId === undefined) {
+                        respondWithError(res, Errors.POST_NOT_FOUND);
+                        return;
+                    }
+                    db.post_like.create({
+                        data: { postId: postId, userId: req.auth.id }
+                    }).then(like => {
+                        res.status(200).send({ postId: postId, liked: true });
+                    }).catch(e => {
+                        if (e.code === PrismaErrorCodes.UNIQUE_CONSTRAINT_FAILED) {
+                            return db.post_like.delete({ where: { postId_userId: { postId: postId, userId: req.auth.id } } })
+                        } else if (e.code === PrismaErrorCodes.FOREIGN_KEY_CONSTRAINT_FAILED) {
+                            respondWithError(res, Errors.POST_NOT_FOUND);
+                        }
+                        else {
+                            handleInternalError(res, e);
+                        }
+                    }).then(removedLike => {
+                        if(removedLike) res.status(200).send({ postId: postId, liked: false });
+                    }).catch(e => handleInternalError(res, e));
+                }
+            }
+        }
+    },
+    comment: {
+        methods: {
+            POST: {
+                /**
+                * 
+                * @param {import('express').Request} req 
+                * @param {import('express').Response} res 
+                */
+                toggle: function (req, res) {
+                    const postId = converters.integer(req.params.postId);
+                    const commentId = converters.integer(req.params.commentId);
+                    if (postId === undefined) {
+                        respondWithError(res, Errors.POST_NOT_FOUND);
+                        return;
+                    }
+                    if (commentId === undefined) {
+                        respondWithError(res, Errors.COMMENT_NOT_FOUND);
+                        return;
+                    }
+                    db.comment_like.create({
+                        data: { commentId: commentId, userId: req.auth.id }
+                    }).then(like => {
+                        res.status(200).json({ commentId: commentId, liked: true });
+                    }).catch(e => {
+                        if (e.code === PrismaErrorCodes.UNIQUE_CONSTRAINT_FAILED) {
+                            return db.comment_like.delete({ where: { commentId_userId: { commentId: commentId, userId: req.auth.id } } })
+                        } else if (e.code === PrismaErrorCodes.FOREIGN_KEY_CONSTRAINT_FAILED) {
+                            respondWithError(res, Errors.COMMENT_NOT_FOUND);
+                        }
+                        else {
+                            return Promise.reject(e);
+                        }
+                    }).then(removedLike => {
+                        if(removedLike) res.status(200).json({ commentId: commentId, liked: false });
+                    }).catch(e => handleInternalError(res, e));
+                }
+            }
+        }
+    }
+}
+
+router.post('/api/newsfeed/posts/:postId/likes::method',
+    auth.authenticate,
+    routeObjectFunctions('method', likes.post.methods.POST),
+);
 
 
+router.post('/api/newsfeed/posts/:postId/comments/:commentId/likes::method',
+    auth.authenticate,
+    routeObjectFunctions('method', likes.comment.methods.POST),
+);
 
 router.get('/api/newsfeed/posts',
     auth.authenticate,
@@ -424,50 +479,43 @@ router.get('/api/newsfeed/posts',
 );
 
 router.post('/api/newsfeed/posts::method',
-
     auth.authenticate,
     routeObjectFunctions('method', posts.methods.POST),
 );
-router.get('/api/newsfeed/posts::method',
 
+router.get('/api/newsfeed/posts::method',
     auth.authenticate,
     routeObjectFunctions('method', posts.methods.GET),
 );
 
 router.get('/api/newsfeed/posts/:postId',
-
     auth.authenticate,
     posts.id.GET,
 );
 
 router.delete('/api/newsfeed/posts/:postId',
-
     auth.authenticate,
     posts.id.DELETE,
 );
 
 router.post('/api/newsfeed/posts/:postId/comments::method',
-
     auth.authenticate,
     routeObjectFunctions('method', comments.methods.POST),
 );
 
 router.get('/api/newsfeed/posts/:postId/comments',
-
     auth.authenticate,
     comments.GET
 );
 
 router.get('/api/newsfeed/posts/:postId/comments/:commentId',
-
     auth.authenticate,
-    comments.commentId.GET,
+    comments.id.GET,
 );
 
 router.delete('/api/newsfeed/posts/:postId/comments/:commentId',
-
     auth.authenticate,
-    comments.commentId.DELETE,
+    comments.id.DELETE,
 );
 
 exports.router = router;
